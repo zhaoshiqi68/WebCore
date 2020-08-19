601fd05752aa3e7ffcbd8f43c04229814ce91e78
zhaoshiqi <zhaoshiqi@beisen.com>
2020-08-19 15:16:08 +0800

    构建镜像名称不能有大写

--- setup.sh
+++ setup.sh
@@ -1,13 +1,13 @@
 image_version=`date +%Y%m%d%H%M`;
 echo $image_version;
-# cd VwbCoreIncludeDocker
+# cd vwbcoreincludedocker
 git pull --rebase origin master;
-docker stop VwbCoreIncludeDocker
-docker rm VwbCoreIncludeDocker
-docker build -t VwbCoreIncludeDocker:$image_version .;
+docker stop vwbcoreincludedocker
+docker rm vwbcoreincludedocker
+docker build -t vwbcoreincludedocker:$image_version .;
 docker images;
-docker run -p 10001:80 -d --name VwbCoreIncludeDocker VwbCoreIncludeDocker:$image_version;
+docker run -p 10001:80 -d --name vwbcoreincludedocker vwbcoreincludedocker:$image_version;
 # -v ~/docker-data/house-web/appsettings.json:/app/appsettings.json -v ~/docker-data/house-web/NLogFile/:/app/NLogFile   --restart=always
-docker logs VwbCoreIncludeDocker;
+docker logs vwbcoreincludedocker;
 #删除build过程中产生的镜像    #docker image prune -a -f
 docker rmi $(docker images -f "dangling=true" -q)
