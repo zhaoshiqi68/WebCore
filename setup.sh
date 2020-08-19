5d0599876e9e0bd07ea18297d77e7a8fd58a2200
zhaoshiqi <zhaoshiqi@beisen.com>
2020-08-19 16:35:04 +0800

    修改setup.sh

--- setup.sh
+++ setup.sh
@@ -1,13 +1,12 @@
-image_version=`date +%Y%m%d%H%M`;
-echo $image_version;
-# cd vwbcoreincludedocker
-git pull --rebase origin master;
-docker stop vwbcoreincludedocker
-docker rm vwbcoreincludedocker
-docker build -t vwbcoreincludedocker:$image_version .;
-docker images;
-docker run -p 10001:80 -d --name vwbcoreincludedocker vwbcoreincludedocker:$image_version;
-# -v ~/docker-data/house-web/appsettings.json:/app/appsettings.json -v ~/docker-data/house-web/NLogFile/:/app/NLogFile   --restart=always
-docker logs vwbcoreincludedocker;
-#删除build过程中产生的镜像    #docker image prune -a -f
-docker rmi $(docker images -f "dangling=true" -q)
+#!/bin/bash
+echo Linux Docker build
+
+cd ../VwbCoreIncludeDocker
+
+dotnet publish -c Release -o ../publish
+
+cd ../publish
+
+echo publish success
+
+docker build -t vwbcoreincludedocker 
